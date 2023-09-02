9a11fb50e4ae5ee8fc1b24fb9e2b7f5c0e07781d
Patricia Louise Ramos <ramospatricialouise@gmail.com>
2023-09-02 21:43:13 +0800

    Initial commit

---
+++ backend/src/app.module.ts
@@ -0,0 +1,36 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { JwtModule } from '@nestjs/jwt';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { secret } from './utils/constants';
+import { join } from 'path';
+import { MulterModule } from '@nestjs/platform-express';
+import { diskStorage } from 'multer';
+import { v4 as uuidv4 } from 'uuid';
+
+@Module({
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost:27017/Stream'),
+    JwtModule.register({
+      secret,
+      signOptions: { expiresIn: '2h' },
+    }),
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'public'),
+    }),
+    MulterModule.register({
+      storage: diskStorage({
+        destination: './public',
+        filename: (req, file, cb) => {
+          const ext = file.mimetype.split('/')[1];
+          cb(null, `${uuidv4()}-${Date.now()}.${ext}`);
+        },
+      }),
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
